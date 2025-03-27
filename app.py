95f7a96e006922787482f6f6aeae015f4fb7d43d
Mateo Sandoval <ksandoval712@unab.edu.co>
2025-03-27 08:19:42 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ import numpy as np
 
 
 # Cargar los modelos
-scaler = joblib.load("scaler.pkl")
-svc_model = joblib.load("svc_model.pkl")
+scaler = joblib.load("scaler (1).bin")
+svc_model = joblib.load("svc_model (1).bin")
 
 # Título y subtítulo
 st.title("Modelo predicción de deserción universitaria con IA")
